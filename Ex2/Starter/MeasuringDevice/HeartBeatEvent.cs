41343680109465489579f0b37ef5e5e314b54b26
Владимир <vovik190798@gmail.com>
2024-04-22 09:22:43 +0300

    4len

---
+++ Ex2/Starter/MeasuringDevice/HeartBeatEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MeasuringDevice
+{
+    public class HeartBeatEventArgs : EventArgs
+    {
+        public DateTime TimeStamp { get; private set; }
+
+        public HeartBeatEventArgs()
+            : base()
+        {
+            this.TimeStamp = DateTime.Now;
+        }
+
+        // Delegate defining the HeartBeat event signature.
+        public delegate void HeartBeatEventHandler(object sender, HeartBeatEventArgs args);
+    }
+}
---
+++ Ex2/Starter/MeasuringDevice/IEventEnabledMeasuringDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static MeasuringDevice.HeartBeatEventArgs;
+
+namespace MeasuringDevice
+{
+    interface IEventEnabledMeasuringDevice : IMeasuringDevice
+    {
+        event EventHandler NewMeasurementTaken;
+
+        // Event that fires every heartbeat.
+        event HeartBeatEventHandler HeartBeat;
+
+        // Read only heartbeat interval - set in constructor.
+        int HeartBeatInterval { get; }
+    }
+}
